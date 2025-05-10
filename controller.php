3addd55273144fdb2877c0b503a8b7151b81f785
gyozasushi <denisabizar54@gmail.com>
2025-05-10 23:59:21 +0700

    add controller.php

--- controller.php
+++ controller.php
@@ -1,12 +1,11 @@
 <?php
 include 'koneksi.php';
 
-// Ambil data dari form
+
 $title       = $_POST['title'];
 $content     = $_POST['content'];
 $category_id = $_POST['category_id'];
 
-// Proses upload gambar
 $gambar_name = $_FILES['image']['name'];
 $tmp_name    = $_FILES['image']['tmp_name'];
 $folder      = "images/";
@@ -14,7 +13,6 @@ $folder      = "images/";
 $upload_path = $folder . basename($gambar_name);
 move_uploaded_file($tmp_name, $upload_path);
 
-// Simpan ke database
 $sql = "INSERT INTO berita (judul, konten, image, category_id, created_at)
         VALUES (?, ?, ?, ?, NOW())";
 
