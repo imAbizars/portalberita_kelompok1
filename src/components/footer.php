ddef145f07a221a401e3479eae0d6c805925007a
gyozasushi <denisabizar54@gmail.com>
2025-06-06 19:44:43 +0700

    edit_footer

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,22 +1,48 @@
 <?php
 include "../db/koneksi.php";
 
-$query = "SELECT id,nama_kategori FROM kategori";
+$query = "SELECT id, nama_kategori FROM kategori";
 $resultkategori = $conn->query($query);
+
+$kategoriChunks = [];
+$temp = [];
+$counter = 0;
+
+while ($row = $resultkategori->fetch_assoc()) {
+    $temp[] = $row;
+    $counter++;
+    if ($counter % 4 == 0) {
+        $kategoriChunks[] = $temp;
+        $temp = [];
+    }
+}
+if (!empty($temp)) {
+    $kategoriChunks[] = $temp;
+}
 ?>
 
 <div class="footer">
     <div class="box-footer">
-        <ul>
-            <?php while($row = $resultkategori->fetch_assoc()): ?>
-                <li>
-                    <a href="berita.php?kategori=<?= urlencode($row['nama_kategori']) ?>">
-                        <?= htmlspecialchars($row['nama_kategori']) ?>
-                    </a>
-                </li>
-            <?php endwhile; ?>
+        <ul class="footer-column">
+            <li><a href="../pages/home.php">Beranda</a></li>
+            <li><a href="../pages/tentangkami.php">Tentang Kami</a></li>
+            <li><a href="../pages/faq.php">FAQ</a></li>
+            <li><a href="../pages/kontak.php">Kontak</a></li>
         </ul>
+
+        <?php foreach ($kategoriChunks as $chunk): ?>
+            <ul class="footer-column">
+                <?php foreach ($chunk as $kategori): ?>
+                    <li>
+                        <a href="berita.php?kategori=<?= urlencode($kategori['nama_kategori']) ?>">
+                            <?= htmlspecialchars($kategori['nama_kategori']) ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endforeach; ?>
     </div>
+
     <div class="copyright">
         &copy; 2025 by kelompok4
     </div>
