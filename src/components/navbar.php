3d5f2aa419eefb63ff7e4be8d030790347680df6
gyozasushi <denisabizar54@gmail.com>
2025-06-08 23:53:21 +0700

    added button logout for navbar

--- src/components/navbar.php
+++ src/components/navbar.php
@@ -2,13 +2,19 @@
 <nav>
     <h2>Portal Berita</h2>
     <ul>
-      <li><a href="../pages/home.php">Beranda</a></li>
+        <li><a href="../pages/home.php">Beranda</a></li>
+        <li><a href="">Berita</a></li>
       <li><a href="../pages/tentangkami.php">Tentang Kami</a></li>
       <li><a href="faq.php">FAQ</a></li>
     </ul>
 
     <?php if (isset($_SESSION['user'])): ?>
-        <span style="color:white;">Halo, <?= htmlspecialchars($_SESSION['user']) ?>!</span>
+        <div class="user-dropdown">
+            <span class="user-name">Halo, <?= htmlspecialchars($_SESSION['user']) ?> &#9662;</span>
+            <div class="logout-menu">
+                <a class="logout-nav" href="../utils/logout.php">Logout</a>
+            </div>
+        </div>
     <?php else: ?>
         <a class="register" href="../pages/register.php">Register</a>
         <a class="sign-in" href="../pages/login.php">Sign-In</a>
